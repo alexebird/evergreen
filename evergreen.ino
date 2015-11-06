9ccae0b19d3eac45f7de6f7e3ac090bdc5f94a9e
Alex Bird <alexebird@gmail.com>
2015-11-05 23:54:07 -0800

    [wip] auto install

--- evergreen.ino
+++ evergreen.ino
@@ -63,7 +63,7 @@ void setupPins() {
 }
 
 int bootEvergreen() {
-  return runShellCmd("curl -k https://raw.githubusercontent.com/alexebird/evergreen/master/bin/evergreen.sh | ash > /tmp/log-evergreen.txt 2>&1");
+  return runShellCmd("curl -k https://raw.githubusercontent.com/alexebird/evergreen/master/bin/evergreen.sh | ash > /tmp/log/boot.log 2>&1");
   //return runShellCmd("ls");
 }
 
