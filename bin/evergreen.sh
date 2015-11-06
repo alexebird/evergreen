594284ebacc07fa20806bc5c3d5ad714a8861658
Alex Bird <alexebird@gmail.com>
2015-11-05 22:14:38 -0800

    [wip] auto install

--- bin/evergreen.sh
+++ bin/evergreen.sh
@@ -9,23 +9,27 @@ mothership='mothership.alxb.us'
 
 cd /root
 
-opkg update
-opkg install curl ncat bind-dig ruby ruby-json
+exit_code=1
+
+opkg update && opkg install curl ncat bind-dig ruby ruby-json || exit $exit_code
+i=$((i+1))
 
 if [ ! -f '/root/resolv.conf' ]; then
   echo 'nameserver 8.8.8.8' > /root/resolv.conf
   ln -f -s /root/resolv.conf /etc/resolv.conf
 fi
 
-ping -c2 $mothership || exit 1
+ping -c2 $mothership || exit $exit_code
+i=$((i+1))
 
 rm -rf evergreen
-git clone git://github.com/alexebird/evergreen.git || exit 2
+git clone git://github.com/alexebird/evergreen.git || exit $exit_code
+i=$((i+1))
 
 cd evergreen
 local ifconfig_f='/root/ifconfig.txt'
 ifconfig | tr '\n' '$' > $ifconfig_f
-curl -XPOST $mothership:8889/arduino -d @$ifconfig_f || echo could send ifconfig
+curl -XPOST $mothership:8889/arduino -d @$ifconfig_f || echo couldnt send ifconfig
 rm -f $ifconfig_f
 
 echo starting server...
