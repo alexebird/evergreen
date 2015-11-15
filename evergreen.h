05d5d68664c640daee16eea7d1a3214f694eb1bf
Alex Bird <alexebird@gmail.com>
2015-11-14 16:18:56 -0800

    get all button sequences working

--- evergreen.h
+++ evergreen.h
@@ -1,20 +1,35 @@
-typedef unsigned int btn_mask;
+typedef unsigned long btn_mask;
 typedef unsigned long tyme;
 
-#define MASK_WIDTH 2
+#define BTN_MASK_SIZE 32
+#define MASK_WIDTH 4
 
-#define HOLD_DOWN_MASK 0b00
-#define BLU_MASK       0b10
-#define RED_MASK       0b01
+#define NULL_MASK 0b0000
+#define BLU_MASK  0b1000
+#define RED_MASK  0b0100
+#define ALL_MASK  0b1100
+#define HOLD_MASK 0b0001
 
-#define MASK_BLU_POINT 0b0000000000000010
-#define MASK_RED_POINT 0b0000000000000001
-#define MASK_NEW_GAME1 0b0000000000111100
-#define MASK_NEW_GAME2 0b0000000001111100
-#define MASK_NEW_GAME3 0b0000000010111100
+#define MASK_BLU_INC_POINT   ( (BLU_MASK << MASK_WIDTH) | NULL_MASK )
+#define MASK_BLU_DEC_POINT   ( (BLU_MASK << MASK_WIDTH*2) | (ALL_MASK << MASK_WIDTH) | BLU_MASK )
+
+#define MASK_RED_INC_POINT   ( (RED_MASK << MASK_WIDTH) | NULL_MASK )
+#define MASK_RED_DEC_POINT   ( (RED_MASK << MASK_WIDTH*2) | (ALL_MASK << MASK_WIDTH) | RED_MASK )
+
+//#define MASK_RED_POINT 0b0000000000000001
+#define MASK_NEW_GAME1   (ALL_MASK | HOLD_MASK)
+#define MASK_NEW_GAME2   ( (BLU_MASK << MASK_WIDTH) | (ALL_MASK | HOLD_MASK) )
+#define MASK_NEW_GAME3   ( (RED_MASK << MASK_WIDTH) | (ALL_MASK | HOLD_MASK) )
+//#define MASK_NEW_GAME2 0b0000000001111100
+//#define MASK_NEW_GAME3 0b0000000010111100
 #define HOLD_DOWN_MILLIS 2000
 
-#define MK_MASK(BLU, RED)     ( ((BLU == HIGH) << 1) | (RED == HIGH) )
-#define LAST_MASK(SEQ)        ( SEQ & 0b11 )
+#define MASK_x1 0b1111
+#define MASK_x2 0b11111111
+#define MASK_x3 0b111111111111
+
+#define MK_MASK(BLU, RED)     ( (((BLU == HIGH) << 1) | (RED == HIGH)) << 2 )
+#define LAST_MASK(SEQ)        ( SEQ & 0b1111 )
 #define SEQ_MATCH(SEQ, MASK)  ( LAST_MASK(SEQ) == MASK )
 #define SEQ_PUSH(SEQ, MASK)   ( (SEQ << MASK_WIDTH) | MASK )
+#define SEQ_CLEAR_HOLD_BIT(SEQ)  ( SEQ & ~0b1 )
--- evergreen.ino
+++ evergreen.ino
@@ -10,6 +10,8 @@ const int grnLedPin    = 13;
 int currBlu = 0;
 int currRed = 0;
 
+boolean detected = false;
+
 #define DEBUG_MASK(n, m) \
     Serial.print(n); \
     Serial.print(": 0b"); \
@@ -101,60 +103,120 @@ void loop() {
     /*DEBUG_MASK("seq", btn_seq);*/
     /*Serial.print("\n\n");*/
 
-    delay(200);
+    /*delay(2);*/
 }
 
-void printButtonChange(btn_mask mask) {
-    if (mask == (BLU_MASK | RED_MASK)) {
-        Serial.println("both down");
+void print_seq(btn_mask seq) {
+    Serial.print("seq: ");
+    int mask = 0;
+    for (int i = 1; i <= (BTN_MASK_SIZE / MASK_WIDTH); i++) {
+        mask = (seq >> (BTN_MASK_SIZE - (MASK_WIDTH * i))) & 0b1111;
+        if (mask < 0b10) {
+            Serial.print("000");
+        }
+        else if (mask < 0b100) {
+            Serial.print("00");
+        }
+        else if (mask < 0b1000) {
+            Serial.print("0");
+        }
+        Serial.print(mask, BIN);
+        Serial.print(" ");
     }
-    else if (mask == BLU_MASK) {
-        Serial.println("blu down");
-    }
-    else if (mask == RED_MASK) {
-        Serial.println("red down");
-    }
+    Serial.print("\n");
 }
+
+/*void printButtonChange(btn_mask mask) {*/
+    /*if (mask == (BLU_MASK | RED_MASK)) {*/
+        /*Serial.println("both down");*/
+    /*}*/
+    /*else if (mask == BLU_MASK) {*/
+        /*Serial.println("blu down");*/
+    /*}*/
+    /*else if (mask == RED_MASK) {*/
+        /*Serial.println("red down");*/
+    /*}*/
+/*}*/
 
 /*
  * the ignore mask is for ignoring curr_mask changes.
  */
-btn_mask ignore_mask = 0b00;
+/*btn_mask ignore_mask = 0b1111;*/
 
 btn_mask handleBtns(btn_mask seq, int currBlu, int currRed) {
     btn_mask curr_mask = MK_MASK(currBlu, currRed);
-    DEBUG_MASK("curr_mask", curr_mask);
-    DEBUG_MASK("seq", seq);
+    /*DEBUG_MASK("curr_mask", curr_mask);*/
+    /*DEBUG_MASK("seq", seq);*/
 
-    /*if (curr_mask == 0) {*/
+    /*if (curr_mask == NULL_MASK) {*/
+        /*seq = SEQ_PUSH(seq, NULL_MASK);*/
         /*return seq;*/
     /*}*/
 
-    if (!SEQ_MATCH(seq, curr_mask)) {
-        printButtonChange(curr_mask);
+    if (!SEQ_MATCH(SEQ_CLEAR_HOLD_BIT(seq), curr_mask)) {
+        detected = false;
+        /*printButtonChange(curr_mask);*/
         seq = SEQ_PUSH(seq, curr_mask);
-        DEBUG_MASK("seq", seq);
-        /*last_seq_change = millis();*/
+        last_seq_change = millis();
     }
-    /*else {*/
-        /*curr_time = millis();*/
-        /*if ((curr_time - last_seq_change) > HOLD_DOWN_MILLIS) {*/
-            /*Serial.println("2 sec");*/
-            /*//seq = SEQ_PUSH(seq, curr_mask);*/
-            /*seq = SEQ_PUSH(seq, HOLD_DOWN_MASK);*/
-        /*}*/
-    /*}*/
+    else {
+        curr_time = millis();
+        if ((curr_time - last_seq_change) > HOLD_DOWN_MILLIS) {
+            if (LAST_MASK(seq) != NULL_MASK && (LAST_MASK(seq) & HOLD_MASK) == 0) {
+                detected = false;
+                seq |= HOLD_MASK;
+            }
+        }
+    }
 
-    /*seq = btn_seq_action(seq, curr_mask);*/
+    if (!detected) {
+        seq = btn_seq_action(seq);
+    }
 
+    /*print_seq(seq);*/
     return seq;
 }
 
-btn_mask btn_seq_action(btn_mask seq, btn_mask mask) {
-    /*for (int i = 0 ; i < size(foo); i ++) {*/
+btn_mask btn_seq_action(btn_mask seq) {
+    if ((seq & MASK_x3) == MASK_BLU_INC_POINT) {
+        detected = true;
+        Serial.println("blu inc point");
+        /*seq = SEQ_PUSH(seq, NULL_MASK);*/
+        /*return 0b0;*/
+    }
+    else if ((seq & MASK_x3) == MASK_BLU_DEC_POINT) {
+        detected = true;
+        Serial.println("blu dec point");
+        /*seq = SEQ_PUSH(seq, NULL_MASK);*/
+        /*return 0b0;*/
+    }
+    else if ((seq & MASK_x3) == MASK_RED_INC_POINT) {
+        detected = true;
+        Serial.println("red inc point");
+        /*seq = SEQ_PUSH(seq, NULL_MASK);*/
+        /*return 0b0;*/
+    }
+    else if ((seq & MASK_x3) == MASK_RED_DEC_POINT) {
+        detected = true;
+        Serial.println("red dec point");
+        /*seq = SEQ_PUSH(seq, NULL_MASK);*/
+        /*return 0b0;*/
+    }
+    else if (((seq & MASK_x2) == MASK_NEW_GAME1) ||
+             ((seq & MASK_x3) == MASK_NEW_GAME2) ||
+             ((seq & MASK_x3) == MASK_NEW_GAME3)) {
+        detected = true;
+        Serial.println("new game");
+        /*seq = SEQ_PUSH(seq, NULL_MASK);*/
+        /*return 0b0;*/
+    }
+    /*else {*/
+        /*return seq;*/
     /*}*/
+
     /*flashNTimes(3);*/
     /*digitalWrite(grnLedPin, HIGH);*/
+
     return seq;
 }
 
