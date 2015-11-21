5122b20948e312f985d2f1513e7a0842576c7354
Alex Bird <alexebird@gmail.com>
2015-11-20 23:59:04 -0800

    add endpoints

--- kiosk/kiosk.rb
+++ kiosk/kiosk.rb
@@ -2,11 +2,122 @@
 
 require './bundle/bundler/setup'
 
+require 'json'
 require 'sinatra'
 
-class Scoreboard
+module Util
+  extend self
+
+  def utc_ts
+    Time.now.utc
+  end
 end
 
-get '/foo' do
-  puts 'foo'
+class Point
+  attr_reader :value, :created_at
+
+  def initialize(value)
+    @value = value
+    @created_at = Util.utc_ts
+  end
+
+  def as_json
+    {
+      value: @value,
+      created_at: @created_at.to_s,
+    }
+  end
+
+  def self.inc
+    new(1)
+  end
+
+  def self.dec
+    new(-1)
+  end
+end
+
+class Player
+  def initialize(color, name=nil)
+    @color = color
+    @name = name || "#{color} player"
+    @points = []
+  end
+
+  def inc
+    @points << Point.inc
+  end
+
+  def dec
+    @points << Point.dec
+  end
+
+  def score
+    @points.reduce(0) do |m,v|
+      m + v.value
+    end
+  end
+
+  def as_json
+    {
+      color: @color,
+      name: @name,
+      points: @points.map(&:as_json),
+    }
+  end
+end
+
+class ScoreboardGame
+  def initialize(blue=Player.new('blue'),red=Player.new('red'))
+    @created_at = @updated_at = Util.utc_ts
+    @players = {
+      blue: blue,
+      red: red,
+    }
+  end
+
+  def update!
+    @updated_at = Util.utc_ts
+  end
+
+  def player_action(color, action)
+    update!
+    @players[color.to_sym].send(action.to_sym)
+  end
+
+  def to_json
+    JSON.dump(
+      {
+        'created_at' => @created_at.to_s,
+        'updated_at' => @updated_at.to_s,
+        'players' => @players.values.map(&:as_json),
+      }
+    )
+  end
+end
+
+$game = nil
+
+def new_game
+  $game = ScoreboardGame.new
+end
+
+def game
+  $game ||= new_game
+end
+
+def render_game(code)
+  content_type :json
+  status code
+  game.to_json
+end
+
+post '/scoreboard_game' do
+  new_game
+  render_game 201
+end
+
+put '/scoreboard_game/:color/:point_action' do
+  game.player_action(params[:color], params[:point_action])
+  render_game 200
 end
